422f14eeaa40f607ba19e613c0f64be77ebd5935
Matt Lauer <mlauer2015@gmail.com>
2024-12-15 14:46:09 -0600

    Updated BezierSurface doc plots

--- aerocaps/scripts/make_surface_plots.py
+++ aerocaps/scripts/make_surface_plots.py
@@ -25,10 +25,11 @@ def plot_bezier_surf_2x3(image_dir: str = None):
     surf.plot_surface(plot, color="#4d86b8", opacity=0.5)
     surf.plot_control_points(plot, render_points_as_spheres=True, point_size=20, color="black")
     surf.plot_control_point_mesh_lines(plot, color="#d6693a")
-    plot.camera.zoom(1.8)
+    plot.camera.position = [-0.5, -0.7, 0.5]
+    plot.camera.focal_point = [0.6, 0.5, -0.05]
 
     if image_dir is not None:
-        plot.screenshot(os.path.join(image_dir, "bezier_surf_2x3.png"), scale=2)
+        plot.screenshot(os.path.join(image_dir, "bezier_surf_2x3.png"), scale=1)
     else:
         plot.show()
 
@@ -49,17 +50,21 @@ def plot_bezier_surf_2x3_mesh_only(image_dir: str = None):
     surf = aerocaps.examples.bezier_surface.bezier_surface_2x3()
 
     surf.plot_surface(plot, color="#4d86b8", opacity=0.5, show_edges=True)
-    plot.camera.zoom(1.75)
+    plot.add_arrows(np.array([[0.1, 0.1, 0.05], [0.1, 0.1, 0.05]]), np.array([[0.0, 1.0, 0.0], [1.0, 0.0, 0.0]]),
+                    mag=0.2, color="black")
+    plot.add_point_labels(np.array([[0.1, 0.32, 0.05], [0.32, 0.1, 0.05]]), ["u", "v"], font_size=32)
+    plot.camera.position = [-0.5, -0.7, 0.5]
+    plot.camera.focal_point = [0.6, 0.5, -0.05]
 
     if image_dir is not None:
-        plot.screenshot(os.path.join(image_dir, "bezier_surf_2x3_mesh_only.png"), scale=2)
+        plot.screenshot(os.path.join(image_dir, "bezier_surf_2x3_mesh_only.png"), scale=1)
     else:
         plot.show()
 
 
 def main(*args, **kwargs):
-    # plot_bezier_surf_2x3(*args, **kwargs)
-    plot_bezier_surf_2x3_mesh_only(*args, **kwargs)
+    plot_bezier_surf_2x3(*args, **kwargs)
+    # plot_bezier_surf_2x3_mesh_only(*args, **kwargs)
 
 
 if __name__ == "__main__":
