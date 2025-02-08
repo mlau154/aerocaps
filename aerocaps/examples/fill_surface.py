732da6c33c6e2722e212d7083a3e20be35840fed
mlauer154 <mlauer2@illinois.edu>
2025-02-08 17:53:57 -0600

    Corrected fill surface example

--- aerocaps/examples/fill_surface.py
+++ aerocaps/examples/fill_surface.py
@@ -5,7 +5,7 @@ import numpy as np
 import aerocaps as ac
 
 
-def fill_surface_four_sided() -> (ac.RationalBezierSurface, typing.List[ac.BezierCurve3D or ac.RationalBezierCurve3D]):
+def fill_surface_four_sided(displacement_degree: int = 3) -> (ac.RationalBezierSurface, typing.List[ac.BezierCurve3D or ac.RationalBezierCurve3D]):
     """
     Creates a four-sided fill surface from a combination of rational and non-rational Bézier curves.
 
@@ -15,12 +15,17 @@ def fill_surface_four_sided() -> (ac.RationalBezierSurface, typing.List[ac.Bezie
 
         Fill surface from four curve boundaries
 
+    Parameters
+    ----------
+    displacement_degree: int
+        Displacement degree to use for the fill surface top and bottom boundary insertion
+
     Returns
     -------
     ac.RationalBezierSurface, typing.List[ac.Bezier3D or ac.RationalBezierCurve3D]
         Fill surface and the list of the four boundary curves
     """
-    left_curve = ac.RationalBezierCurve3D.generate_from_array(
+    left_curve = ac.RationalBezierCurve3D(
         np.array([
             [0.0, 0.0, 0.0],
             [0.5, 0.5, 0.0],
@@ -28,13 +33,13 @@ def fill_surface_four_sided() -> (ac.RationalBezierSurface, typing.List[ac.Bezie
         ]),
         np.array([1.0, 0.7, 1.0])
     )
-    right_curve = ac.BezierCurve3D.generate_from_array(
+    right_curve = ac.BezierCurve3D(
         np.array([
             [2.0, 0.1, 0.0],
             [2.1, 0.9, 0.0]
         ])
     )
-    top_curve = ac.BezierCurve3D.generate_from_array(
+    top_curve = ac.BezierCurve3D(
         np.array([
             left_curve.control_points[-1].as_array(),
             [0.5, 1.2, 0.0],
@@ -42,7 +47,7 @@ def fill_surface_four_sided() -> (ac.RationalBezierSurface, typing.List[ac.Bezie
             right_curve.control_points[-1].as_array()
         ])
     )
-    bottom_curve = ac.BezierCurve3D.generate_from_array(
+    bottom_curve = ac.BezierCurve3D(
         np.array([
             left_curve.control_points[0].as_array(),
             [1.2, 0.2, 0.0],
@@ -50,6 +55,7 @@ def fill_surface_four_sided() -> (ac.RationalBezierSurface, typing.List[ac.Bezie
         ])
     )
     return (
-        ac.RationalBezierSurface.fill_surface_from_four_boundaries(left_curve, right_curve, top_curve, bottom_curve),
+        ac.RationalBezierSurface.fill_surface_from_four_boundaries(left_curve, right_curve, top_curve, bottom_curve,
+                                                                   displacement_degree=displacement_degree),
         [left_curve, right_curve, top_curve, bottom_curve]
     )
